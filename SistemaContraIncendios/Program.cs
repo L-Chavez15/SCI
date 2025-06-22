9ae8b9329fd51fcb168e11e15907b41e73db9797
Leonardo Chávez <n00449821pn.pe>
2025-06-21 20:33:45 -0500

    Agregar archivos de proyecto.

---
+++ SistemaContraIncendios/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaContraIncendios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void pisos()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                //Console.WriteLine("\t=================================");
+                Console.WriteLine("\t*****SISTEMA CONTRA INCENDIOS*****    ");
+                //Console.WriteLine("\t=================================\n");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\t╔═══════════════════════════════════════╗");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\tPISO 3\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t╠═══════════════════════════════════════╣");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\tPISO 2\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t╠═══════════════════════════════════════╣");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\tPISO 1\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t║\t\t\t\t\t║");
+                Console.WriteLine("\t╚═══════════════════════════════════════╝\n");
+                Console.ResetColor();
+                string opcion;
+                Console.WriteLine("INGRESE EL PISO A INSPECCIONAR:  ");
+                opcion = Console.ReadLine();
+            }
+        }
+    }
+}
